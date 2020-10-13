b7e74a69b692b370f587f26ef771e594f197bfce
AlexanderSchallmeiner <raidyyou@gmail.com>
2020-10-13 10:44:42 +0200

    Started

---
+++ Hausuebung4/src/main/java/j/hausuebung4/Beispiel1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j.hausuebung4;
+
+/**
+ *
+ * @author alexa
+ */
+public class Beispiel1 {
+    
+}
